87030ab951148094d8c7719191b0252c612c1800
Paweł Krzaczkowski <pawel@freshmind.pl>
2019-10-16 11:28:09 +0200

    fix #19

--- tests/CampaignsTest.php
+++ tests/CampaignsTest.php
@@ -136,4 +136,100 @@ class CampaignsTest extends TestCase
         $this->assertEquals('foo', $campaigns->messages([
         ]));
     }
+
+    public function testCampaignGetActions()
+    {
+        $stub = $this->getMockBuilder('Customerio\Client')->disableOriginalConstructor()->getMock();
+        $stub->method('get')->willReturn('foo');
+        $campaigns = new Campaigns($stub);
+        $this->assertEquals('foo', $campaigns->actions([
+            'id' => 1,
+        ]));
+    }
+
+    /**
+     * @expectedException \GuzzleHttp\Exception\RequestException
+     */
+    public function testCampaignGetActionsMissingId()
+    {
+        $stub = $this->getMockBuilder('Customerio\Client')->disableOriginalConstructor()->getMock();
+        $stub->method('get')->willReturn('foo');
+        $campaigns = new Campaigns($stub);
+        $this->assertEquals('foo', $campaigns->actions([
+        ]));
+    }
+
+    public function testCampaignGetAction()
+    {
+        $stub = $this->getMockBuilder('Customerio\Client')->disableOriginalConstructor()->getMock();
+        $stub->method('get')->willReturn('foo');
+        $campaigns = new Campaigns($stub);
+        $this->assertEquals('foo', $campaigns->getAction([
+            'id' => 1,
+            'action_id' => 2
+        ]));
+    }
+
+    /**
+     * @expectedException \GuzzleHttp\Exception\RequestException
+     */
+    public function testCampaignGetActionMissingId()
+    {
+        $stub = $this->getMockBuilder('Customerio\Client')->disableOriginalConstructor()->getMock();
+        $stub->method('get')->willReturn('foo');
+        $campaigns = new Campaigns($stub);
+        $this->assertEquals('foo', $campaigns->getAction([
+            'action_id' => 2
+        ]));
+    }
+
+    /**
+     * @expectedException \GuzzleHttp\Exception\RequestException
+     */
+    public function testCampaignGetActionMissingActionId()
+    {
+        $stub = $this->getMockBuilder('Customerio\Client')->disableOriginalConstructor()->getMock();
+        $stub->method('get')->willReturn('foo');
+        $campaigns = new Campaigns($stub);
+        $this->assertEquals('foo', $campaigns->getAction([
+            'id' => 1
+        ]));
+    }
+
+    public function testCampaignGetActionMetrics()
+    {
+        $stub = $this->getMockBuilder('Customerio\Client')->disableOriginalConstructor()->getMock();
+        $stub->method('get')->willReturn('foo');
+        $campaigns = new Campaigns($stub);
+        $this->assertEquals('foo', $campaigns->getActionMetrics([
+            'id' => 1,
+            'action_id' => 2
+        ]));
+    }
+
+    /**
+     * @expectedException \GuzzleHttp\Exception\RequestException
+     */
+    public function testCampaignGetActionMetricsMissingId()
+    {
+        $stub = $this->getMockBuilder('Customerio\Client')->disableOriginalConstructor()->getMock();
+        $stub->method('get')->willReturn('foo');
+        $campaigns = new Campaigns($stub);
+        $this->assertEquals('foo', $campaigns->getActionMetrics([
+            'action_id' => 2
+        ]));
+    }
+
+    /**
+     * @expectedException \GuzzleHttp\Exception\RequestException
+     */
+    public function testCampaignGetActionMetricsMissingActionId()
+    {
+        $stub = $this->getMockBuilder('Customerio\Client')->disableOriginalConstructor()->getMock();
+        $stub->method('get')->willReturn('foo');
+        $campaigns = new Campaigns($stub);
+        $this->assertEquals('foo', $campaigns->getActionMetrics([
+            'id' => 1
+        ]));
+    }
 }
